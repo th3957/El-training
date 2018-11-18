3f31c35ee23ae5b5f10662df3d9ad95ba96eab4f
TH <tharada0854@gmail.com>
2018-11-18 06:33:04 +0000

    Taskテーブルにカラム一式を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_113925) do
+ActiveRecord::Schema.define(version: 2018_11_16_034624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2018_11_15_113925) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", limit: 255, null: false
+    t.text "contents", null: false
+    t.datetime "deadline", null: false
+    t.integer "priority", null: false
+    t.integer "state", null: false
   end
 
 end
