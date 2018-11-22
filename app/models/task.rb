4873916b322785ac06ba84ad35f785110d017d05
TH <tharada0854@gmail.com>
2018-11-22 07:49:04 +0000

    status,priorityカラム追加に伴いTaskモデルにenum,バリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ApplicationRecord
+  enum status: { before_start: 0, started: 1, finished: 2 }
+  enum priority: { priority_low: 0, priority_middle: 1, priority_high: 2 }
+
   validates :title, presence: true, length: { in: 1..50 }
-  validates :contents, presence: true,length: { in: 1..200 }
+  validates :contents, presence: true, length: { in: 1..200 }
+  validates :status, presence: true, inclusion: { in: Task.statuses.keys }
+  validates :priority, presence: true, inclusion: { in: Task.priorities.keys }
   validates :deadline, presence: true
   validate :deadline_cannot_be_in_the_past
 
