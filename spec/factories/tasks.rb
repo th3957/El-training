99cef02ca32c47374760c9a52ec583c8a508767e
TH <tharada0854@gmail.com>
2018-11-22 07:53:30 +0000

    status,priorityカラム追加に伴いテストを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     title { 'テストケース１のタイトル' }
     contents { 'テストケース１のコンテンツ' }
-    deadline { Time.zone.local(2019,11,11,7,00) }
+    deadline { Time.zone.local(2019,11,10,7,00) }
   end
 
   factory :second_task, class: Task do
@@ -15,5 +15,22 @@ FactoryBot.define do
     title { 'テストケース３のタイトル' }
     contents { 'テストケース３のコンテンツ' }
     deadline { Time.zone.local(2019,10,10,12,00) }
+  end
+end
+
+FactoryBot.modify do
+  factory :task do
+    status { 'started' }
+    priority { 'priority_high' }
+  end
+
+  factory :second_task, class: Task do
+    status { 'finished' }
+    priority { 'priority_low' }
+  end
+
+  factory :third_task, class: Task do
+    status { 'before_start' }
+    priority { 'priority_middle' }
   end
 end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -17,6 +17,8 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit new_task_path
     fill_in 'task_title', with: 'これはタイトルです。'
     fill_in 'task_contents', with: 'これは内容です。'
+    choose 'task_status_finished'
+    choose 'task_priority_priority_low'
     select '2020', from: 'task_deadline_1i'
     select '11月', from: 'task_deadline_2i'
     select '20', from: 'task_deadline_3i'
@@ -26,6 +28,8 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(page).to have_content 'これはタイトルです。'
     expect(page).to have_content 'これは内容です。'
     expect(page).to have_content '2020年11月20日(金) 20時00分'
+    expect(page).to have_content '完了'
+    expect(page).to have_content '低'
   end
 
   scenario "タスク編集のテスト" do
@@ -33,10 +37,13 @@ RSpec.feature "タスク管理機能", type: :feature do
     click_link 'テストケース２のタイトル'
     click_link '編集'
     fill_in 'task_contents', with: '内容を編集しました。'
+    choose 'task_priority_priority_high'
     click_button '編集'
     expect(page).to have_content 'テストケース２のタイトル'
     expect(page).to have_content '内容を編集しました。'
     expect(page).to have_content '2050年11月11日(金) 09時00分'
+    expect(page).to have_content '完了'
+    expect(page).to have_content '高'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
@@ -46,17 +53,17 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "titleが空ならバリデーションが通らない" do
-    task = Task.new(title: '', contents: '失敗テスト', deadline: DateTime.now.end_of_day)
+    task = Task.new(title: '', contents: '失敗テスト', deadline: DateTime.now.end_of_day, status: 'started', priority: 'priority_high')
     expect(task).not_to be_valid
   end
 
   scenario "deadlineが過去ならバリデーションが通らない" do
-    task = Task.new(title: '失敗テスト', contents: '失敗テスト', deadline: DateTime.yesterday)
+    task = Task.new(title: '失敗テスト', contents: '失敗テスト', deadline: DateTime.yesterday, status: 'finished', priority: 'priority_middle')
     expect(task).not_to be_valid
   end
 
-  scenario "titleとcontentとdeadlineに内容が記載されていればバリデーションが通る" do
-    task = Task.new(title: '成功テスト', contents: '成功テスト', deadline: DateTime.now.end_of_day)
+  scenario "すべての項目に内容が記載されていればバリデーションが通る" do
+    task = Task.new(title: '成功テスト', contents: '成功テスト', deadline: DateTime.now.end_of_day, status: 'before_start', priority: 'priority_low')
     expect(task).to be_valid
   end
 
@@ -64,6 +71,6 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit tasks_path
     click_link '終了期限でソートする'
     task_deadlines = all('.task_deadline').map(&:text)
-    expect(task_deadlines).to eq %W(#{'2019年10月10日(木) 12時00分'} #{'2019年11月11日(月) 07時00分'} #{'2050年11月11日(金) 09時00分'})
+    expect(task_deadlines).to eq %W(#{'2019年10月10日(木) 12時00分'} #{'2019年11月10日(日) 07時00分'} #{'2050年11月11日(金) 09時00分'})
   end
 end
