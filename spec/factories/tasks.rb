6287f1b235c0b6a722b2a3699d2b988da8140670
TH <tharada0854@gmail.com>
2018-11-25 09:22:25 +0000

    検索機能実装に伴いテストを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,34 +3,31 @@ FactoryBot.define do
     title { 'テストケース１のタイトル' }
     contents { 'テストケース１のコンテンツ' }
     deadline { Time.zone.local(2019,11,10,7,00) }
+    status { 'started' }
+    priority { 'priority_high' }
   end
 
   factory :second_task, class: Task do
     title { 'テストケース２のタイトル' }
     contents { 'テストケース２のコンテンツ' }
     deadline { Time.zone.local(2050,11,11,9,00) }
+    status { 'finished' }
+    priority { 'priority_low' }
   end
 
   factory :third_task, class: Task do
     title { 'テストケース３のタイトル' }
     contents { 'テストケース３のコンテンツ' }
     deadline { Time.zone.local(2019,10,10,12,00) }
-  end
-end
-
-FactoryBot.modify do
-  factory :task do
-    status { 'started' }
-    priority { 'priority_high' }
-  end
-
-  factory :second_task, class: Task do
-    status { 'finished' }
-    priority { 'priority_low' }
-  end
-
-  factory :third_task, class: Task do
     status { 'before_start' }
     priority { 'priority_middle' }
+  end
+
+  factory :fourth_task, class: Task do
+    title { 'テストケース４のタイトル' }
+    contents { 'テストケース４のコンテンツ' }
+    deadline { Time.zone.local(2020,11,11,6,00) }
+    status { 'before_start' }
+    priority { 'priority_low' }
   end
 end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -5,17 +5,20 @@ RSpec.feature "タスク管理機能", type: :feature do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
     FactoryBot.create(:third_task)
+    FactoryBot.create(:fourth_task)
   end
 
   scenario "タスク一覧のテスト" do
     visit tasks_path
     expect(page).to have_content 'テストケース１のタイトル'
     expect(page).to have_content 'テストケース２のタイトル'
+    expect(page).to have_content 'テストケース３のタイトル'
+    expect(page).to have_content 'テストケース４のタイトル'
   end
 
   scenario "タスク作成のテスト" do
     visit new_task_path
-    fill_in 'task_title', with: 'これはタイトルです。'
+    all('#task_title')[1].set('これはタイトルです。')
     fill_in 'task_contents', with: 'これは内容です。'
     choose 'task_status_finished'
     choose 'task_priority_priority_low'
@@ -46,23 +49,23 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(page).to have_content '高'
   end
 
-  scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+  scenario "タスクが作成日時の昇順に並んでいるかのテスト" do
     visit tasks_path
     task_titles = all('.task_title').map(&:text)
-    expect(task_titles).to eq %w(テストケース１のタイトル テストケース２のタイトル テストケース３のタイトル)
+    expect(task_titles).to eq %w(テストケース４のタイトル テストケース３のタイトル テストケース２のタイトル テストケース１のタイトル)
   end
 
-  scenario "titleが空ならバリデーションが通らない" do
+  scenario "タイトルが空ならバリデーションが通らない" do
     task = Task.new(title: '', contents: '失敗テスト', deadline: DateTime.now.end_of_day, status: 'started', priority: 'priority_high')
     expect(task).not_to be_valid
   end
 
-  scenario "deadlineが過去ならバリデーションが通らない" do
+  scenario "終了期限が過去ならバリデーションが通らない" do
     task = Task.new(title: '失敗テスト', contents: '失敗テスト', deadline: DateTime.yesterday, status: 'finished', priority: 'priority_middle')
     expect(task).not_to be_valid
   end
 
-  scenario "すべての項目に内容が記載されていればバリデーションが通る" do
+  scenario "すべての項目が正しく記載されていればバリデーションが通る" do
     task = Task.new(title: '成功テスト', contents: '成功テスト', deadline: DateTime.now.end_of_day, status: 'before_start', priority: 'priority_low')
     expect(task).to be_valid
   end
@@ -71,6 +74,49 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit tasks_path
     click_link '終了期限でソートする'
     task_deadlines = all('.task_deadline').map(&:text)
-    expect(task_deadlines).to eq %W(#{'2019年10月10日(木) 12時00分'} #{'2019年11月10日(日) 07時00分'} #{'2050年11月11日(金) 09時00分'})
+    expect(task_deadlines).to eq %W(#{'2019年10月10日(木) 12時00分'} #{'2019年11月10日(日) 07時00分'} #{'2020年11月11日(水) 06時00分'} #{'2050年11月11日(金) 09時00分'})
+  end
+
+  scenario "優先順位でソートができているかのテスト" do
+    visit tasks_path
+    click_link '優先順位でソートする'
+    task_priorities = all('.task_priority').map(&:text)
+    expect(task_priorities).to eq %w(高 中 低 低)
+  end
+
+  scenario "タイトル名で絞り込み検索のテスト" do
+    visit tasks_path
+    all('#task_title')[0].set('２のタイトル')
+    click_button '検索'
+    expect(page).to have_content 'テストケース２のタイトル'
+    expect(page).to have_content '2050年11月11日(金) 09時00分'
+    expect(page).to_not have_content 'テストケース１のタイトル'
+    expect(page).to_not have_content 'テストケース３のタイトル'
+    expect(page).to_not have_content 'テストケース４のタイトル'
+  end
+
+  scenario "タイトル名と進捗状況で絞り込み検索のテスト" do
+    visit tasks_path
+    all('#task_title')[0].set('テストケース')
+    select '未着手', from: 'task_status'
+    click_button '検索'
+    expect(page).to have_content 'テストケース３のタイトル'
+    expect(page).to have_content '2019年10月10日(木) 12時00分'
+    expect(page).to have_content 'テストケース４のタイトル'
+    expect(page).to have_content '2020年11月11日(水) 06時00分'
+    expect(page).to_not have_content 'テストケース１のタイトル'
+    expect(page).to_not have_content 'テストケース２のタイトル'
+  end
+
+  scenario "進捗状況と優先順位で絞り込み検索のテスト" do
+    visit tasks_path
+    select '未着手', from: 'task_status'
+    select '低', from: 'task_priority'
+    click_button '検索'
+    expect(page).to have_content 'テストケース４のタイトル'
+    expect(page).to have_content '2020年11月11日(水) 06時00分'
+    expect(page).to_not have_content 'テストケース１のタイトル'
+    expect(page).to_not have_content 'テストケース２のタイトル'
+    expect(page).to_not have_content 'テストケース３のタイトル'
   end
 end
