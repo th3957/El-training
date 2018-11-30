cd2da04b76f4d6b1ee07a3b20c49829c018c6f79
TH <tharada0854@gmail.com>
2018-11-30 10:52:09 +0000

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :email, presence: true, length: { in: 1..20 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+  has_secure_password
+  validates :password, length: { in: 6..20 }
+  before_validation { email.downcase! }
 end
