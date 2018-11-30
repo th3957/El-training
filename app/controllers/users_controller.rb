df543458042e7fa764dec81c3d018e0714289651
TH <tharada0854@gmail.com>
2018-11-30 10:50:27 +0000

    Userコントローラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to user_path(@user), notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
 
   def new
+    @user = User.new
   end
 
-  def 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation
+                                 )
+  end
 end
