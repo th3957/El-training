b93c3af7d8c05b3e786719c3522b65f886f2ee56
TH <tharada0854@gmail.com>
2018-11-30 07:29:38 +0000

    Userテーブルにemailカラムのindexを追加、READMEを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_050203) do
+ActiveRecord::Schema.define(version: 2018_11_30_072440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_050203) do
     t.string "password_digest", limit: 255, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
