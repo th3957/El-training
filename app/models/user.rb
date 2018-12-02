a456665a88e17ebd2ba60b5d4cb94408aa5853e9
TH <tharada0854@gmail.com>
2018-12-02 14:01:42 +0000

    シードデータを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tasks
 
   validates :name, presence: true, length: { in: 1..20 }
-  validates :email, presence: true, length: { in: 1..20 },
+  validates :email, presence: true, length: { in: 1..50 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
             uniqueness: true
   has_secure_password
