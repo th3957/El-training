ef55f084579410df78cda5ddddc5a1056756935a
TH <tharada0854@gmail.com>
2018-12-03 05:04:45 +0000

    ログイン・ログアウト機能実装に伴いテストを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'テストユーザー１のネーム' }
+    email { '111@gmail.com' }
+    password { 'qazwsx' }
+    password_confirmation { 'qazwsx' }
+  end
+
+  factory :second_user, class: User do
+    name { 'テストユーザー２のネーム' }
+    email { '222@gmail.com' }
+    password { 'edc293' }
+    password_confirmation { 'edc293' }
+    initialize_with { User.find_or_create_by(email: email)}
   end
 end
