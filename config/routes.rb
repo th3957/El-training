d93fdd1b659de0a015e255d4b27f67d15c666561
TH <tharada0854@gmail.com>
2018-12-20 10:09:51 +0000

    routesにrootの条件分岐とroleカラム変更アクション用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,32 @@
+class AuthenticatedAdminConstraint
+  def matches?(request)
+    request.session['user_id'].present? && User.find(request.session['user_id']).role == 'role_admin'
+  end
+end
+
+class AuthenticatedUserConstraint
+  def matches?(request)
+    request.session['user_id'].present? && User.find(request.session['user_id']).role != 'role_admin'
+  end
+end
+
 Rails.application.routes.draw do
+  constraints AuthenticatedAdminConstraint.new do
+    root to: 'admin/users#index'
+  end
+
+  constraints AuthenticatedUserConstraint.new do
+    root to: 'tasks#index'
+  end
+
   root to: 'sessions#new'
+
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   namespace :admin do
-    resources :users
+    resources :users do
+      patch 'change_role', on: :member
+    end
   end
 end
