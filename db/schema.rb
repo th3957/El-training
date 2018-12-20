e5c1ba5381bb5ee181c7fe7cd556013384312a73
TH <tharada0854@gmail.com>
2018-12-20 09:29:41 +0000

    Usersテーブルにroleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_124714) do
+ActiveRecord::Schema.define(version: 2018_12_19_073807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_12_02_124714) do
     t.string "password_digest", limit: 255, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
