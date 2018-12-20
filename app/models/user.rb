719bee1264bf4f51f47b69b86a64c0c59b608501
TH <tharada0854@gmail.com>
2018-12-20 09:40:48 +0000

    roleカラム追加に伴いUserモデルにenum,バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation { email.downcase! }
+
   has_many :tasks, dependent: :destroy
+
+  enum role: { role_common: 0, role_admin: 1 }
 
   validates :name, presence: true, length: { in: 1..20 }
   validates :email, presence: true, length: { in: 1..50 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
             uniqueness: true
-  has_secure_password
   validates :password, length: { in: 6..20 }
-  before_validation { email.downcase! }
+  validates :role, presence: true, inclusion: { in: User.roles.keys }
 end
