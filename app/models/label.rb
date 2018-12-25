240e30f8a105085664e7401649ae2949a7d45db3
TH <tharada0854@gmail.com>
2018-12-25 03:42:16 +0000

    Labelモデルにバリデーションを追加

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,6 @@
 class Label < ApplicationRecord
   has_many :labelings, dependent: :destroy
   has_many :labeling_tasks, through: :labelings, source: :task
+
+  validates :title, presence: true, length: { in: 1..10 }
 end
